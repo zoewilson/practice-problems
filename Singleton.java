b8a5aeb7aa7f0319f579991b97392b1cd0e242ca
Zoe Wilson <zoewilson169@gmail.com>
2016-02-28 20:32:52 -0500

    Added Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,13 @@
+/* Singleton */
+
+public class Singleton {
+  private static Singleton instance = null;
+  private Singleton() {}
+
+  public static Singleton getInstance() {
+    if (instance == null) {
+        instance = new Singleton();
+    }
+    return instance;
+  }
+}    
